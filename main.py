cf63a05ca957de0209cc8f6a8db8150575db93fe
Dima <dima.yerosh@gmail.com>
2021-01-06 16:43:40 +0100

    Log functionality to track on remote machine

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from services.weather import get_weather
 from services.covid_updater import construct_message
 from services.telegram import send_telegram_message
 from services.air_quality import get_air_quality
+from log import add_to_log
  
 city = current_city['name']
 lat = current_city['lat']
@@ -11,3 +12,4 @@ lon = current_city['lon']
 base_message = f'{construct_message()}\n{get_weather(city)}\n\n{get_air_quality(lat, lon)}'
 
 send_telegram_message(base_message)
+add_to_log()
